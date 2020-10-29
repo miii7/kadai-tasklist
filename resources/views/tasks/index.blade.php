b03950b1e0d1dbcb2747983fcf8aa8fbc16b5f2d
yuuu <miii888prog@gmail.com>
2020-10-29 08:00:32 +0000

    modify

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,6 +13,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                     <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -20,12 +21,14 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{{ $task->id }}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
+    
     
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
@@ -34,6 +37,7 @@
                 <tr>
                     {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
